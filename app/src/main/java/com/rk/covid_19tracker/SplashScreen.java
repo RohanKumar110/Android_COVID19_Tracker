45773cf01cd15ca01845b5206521731bd8ab8286
= <=>
2020-07-01 00:23:42 +0500

    Splash Screen Added

---
+++ app/src/main/java/com/rk/covid_19tracker/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.rk.covid_19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            }
+        },4000);
+    }
+}
